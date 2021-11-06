35933649aff30d0db1438c9001407c8eea34b1bd
tarunsunny3 <tarunsunny3@gmail.com>
2021-11-06 12:35:28 +0530

    tried again

--- iiith.js
+++ iiith.js
@@ -1,8 +1,8 @@
 const puppeteer = require("puppeteer");
 
 try {
-    (async()=>{
-    const browser = await puppeteer.launch({ headless: false});
+  (async () => {
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     //await page.setDefaultNavigationTimeout(0);
     await page.goto("https://www.iiitb.ac.in/faculty");
@@ -10,15 +10,20 @@ try {
     //await page.waitForSelector("#faculty > tbody > tr:nth-child(1) > td:nth-child(2) > h3 > a");
     //console.log("Hello");
     //console.log(await page.$$("div.faculty-info > h3 > a"));
-    const res = await page.$$eval("div.faculty-info > h3 > a",hrefs =>{ 
-        hrefs.map(a=>{
-            a.href 
-        })
-});
-    console.log(res);
-    })();
+    const res1 = await page.$$("div.faculty-info > h3 > a");
+    await Promise.all();
+
+    // await res1[0].click();
+    // const res = await page.$$eval("div.faculty-info > h3 > a", (hrefs) =>
+    //   hrefs.map((option) => {
+    //     return {
+    //       data: option.innerHTML,
+    //       link: option.href,
+    //     };
+    //   })
+    // );
+    // console.log(res);
+  })();
 } catch (error) {
-    console.error(
-        error
-    );
+  console.error(error);
 }
